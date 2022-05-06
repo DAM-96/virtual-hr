00be4d4bad66ada19ad411221124bde9200ed9fd
DAM-96 <d.a.morenog@outlook.com>
2022-05-05 22:58:36 -0500

    Added basic node configurations and minor advances on index

---
+++ index.js
@@ -0,0 +1,10 @@
+let questions = [
+    {
+        type: "list",
+        name: "initialOperation",
+        message: "What would you like to do?",
+        choices: [
+            
+        ]
+    }
+]
